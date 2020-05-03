aad5fd9c615ff6ad8d0e6c10296336ce0b002360
vvyugovskiy <v.vyugovskiy@mail.com>
2020-05-02 20:33:28 -0400

    grid run

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -1,15 +1,26 @@
 package com.vytrack.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Driver {
 
     //same for everyone
     private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
+//    private static ChromeOptions chromeOptions;
+//    private static FirefoxOptions firefoxOptions;
+//    private static URL url;
 
     //so no one can create object of Driver class
     //everyone should call static getter method instead
@@ -44,16 +55,58 @@ public class Driver {
                     chromeOptions.addArguments("--start-maximized");
                     driverPool.set(new ChromeDriver(chromeOptions));
                     break;
-                case "chromeheadless":
+                case "chrome-headless":
                     //to run chrome without interface (headless mode)
                     WebDriverManager.chromedriver().setup();
-                    ChromeOptions options = new ChromeOptions();
-                    options.setHeadless(true);
-                    driverPool.set(new ChromeDriver(options));
+                    chromeOptions = new ChromeOptions();
+                    chromeOptions.setHeadless(true);
+                    driverPool.set(new ChromeDriver(chromeOptions));
+                    break;
+//                case "remote-chrome":
+//                    chromeOptions = new ChromeOptions();
+//                    try {
+//                        URL url = new URL("http://54.157.252.238:4444/wd/hub");
+//                        driverPool.set(new RemoteWebDriver(url, chromeOptions));
+//                    } catch (MalformedURLException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+                case "chrome-remote":
+                    try {
+                        //we create object of URL and specify
+                        //selenium grid hub as a parameter
+                        //make sure it ends with /wd/hub
+                        URL url = new URL("http://54.157.252.238:4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        //desiredCapabilities used to specify what kind of node
+                        //is required for testing
+                        //such as: OS type, browser, version, etc...
+                        desiredCapabilities.setBrowserName(BrowserType.CHROME);
+                        desiredCapabilities.setPlatform(Platform.ANY);
+                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
+                    break;
+                case "firefox-headless":
+                    //to run chrome without interface (headless mode)
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.setHeadless(true);
+                    driverPool.set(new FirefoxDriver(firefoxOptions));
+                    break;
+                case "remote-firefox":
+                    firefoxOptions = new FirefoxOptions();
+                    try {
+                        URL url = new URL("http://54.157.252.238:4444/wd/hub");
+                        driverPool.set(new RemoteWebDriver(url, firefoxOptions));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     throw new RuntimeException("Wrong browser name!");
